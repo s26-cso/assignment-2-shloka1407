55afacd85c488b7dd18d332491dc0a1131edb82a
Shloka Gupta <shvag222@gmail.com>
2026-04-13 10:49:02 +0000

    q5

--- q5/q5.s
+++ q5/q5.s
@@ -9,38 +9,31 @@ buf2: .space 1
 
 .section .text
 .globl _start
-
 _start:
-
-# open file (fd -> s0)
-li a7,1024
+li a7,1024              # open file (fd -> s0)
 la a0,filename
 li a1,0
 ecall
 mv s0,a0
 
-# get file size using lseek (size -> s1)
-li a7,62
+li a7,62                #get file size 
 mv a0,s0
 li a1,0
 li a2,2
 ecall
 mv s1,a0
 
-# initialize left and right pointers
-li s2,0
+li s2,0                 #initialize left and right pointers
 addi s3,s1,-1
 
 loop:
-# stop if pointers cross
-bge s2,s3,palindrome
+bge s2,s3,palindrome            #stop if pointers cross
 
-# seek to left and read 1 byte
 li a7,62
 mv a0,s0
 mv a1,s2
 li a2,0
-ecall
+ecall                           #seek to left and read 1 byte
 
 li a7,63
 mv a0,s0
@@ -48,8 +41,7 @@ la a1,buf1
 li a2,1
 ecall
 
-# seek to right and read 1 byte
-li a7,62
+li a7,62                    #seek to right and read 1 byte
 mv a0,s0
 mv a1,s3
 li a2,0
@@ -61,19 +53,16 @@ la a1,buf2
 li a2,1
 ecall
 
-# compare characters
-lb t0,buf1
+lb t0,buf1                  #compare characters
 lb t1,buf2
 bne t0,t1,not_pal
 
-# move inward
-addi s2,s2,1
+addi s2,s2,1                #move inward
 addi s3,s3,-1
 j loop
 
 palindrome:
-# print "Yes"
-li a7,64
+li a7,64                #print "Yes"
 li a0,1
 la a1,yes
 li a2,4
@@ -81,15 +70,13 @@ ecall
 j exit
 
 not_pal:
-# print "No"
-li a7,64
+li a7,64                    #print "No"
 li a0,1
 la a1,no
 li a2,3
 ecall
 
 exit:
-# exit program
-li a7,93
+li a7,93                #exit program
 li a0,0
 ecall
